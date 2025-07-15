a96bb65773a7c4dd62e7c6ec4b41bcb1a051d76e
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-15 09:47:57 -0500

    Initial Commit

---
+++ CollabTests/CollabTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollabTests.swift
+//  CollabTests
+//
+//  Created by Tirzaan on 7/15/25.
+//
+
+import Testing
+@testable import Collab
+
+struct CollabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
